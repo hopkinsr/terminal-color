b086a81d4b4c97b5be0f48b1fe2556c8d7898673
Richard Hopkins <hopkinsr@server.fake>
2015-01-23 21:34:05 +0000

    Simplify implementation for 'off

--- terminal-color/private/off.rkt
+++ terminal-color/private/off.rkt
@@ -4,18 +4,15 @@
          print-color
          write-color)
 
-(define (output-color output-method datum out #:fg fg #:bg bg)
-  (output-method datum out))
-
 (define (display-color datum out #:fg fg #:bg bg)
-  (output-color display datum out #:fg fg #:bg bg))
+  (display datum out))
 
 (define (displayln-color datum out #:fg fg #:bg bg)
-  (output-color displayln datum out #:fg fg #:bg bg))
+  (displayln datum out))
 
 (define (print-color datum out quote-depth #:fg fg #:bg bg)
-  (print datum out quote-depth #:fg fg #:bg bg))
+  (print datum out quote-depth))
 
 (define (write-color datum out #:fg fg #:bg bg)
-  (output-color write datum out #:fg fg #:bg bg))
+  (write datum out))
 
